3a3e455ef21a2873f349e0ad0562836a2f6efd38
lewoer <shile4408@126.com>
2019-02-14 15:13:00 +0800

    Assert | Parameterized Testing

--- Banking/src/com/shile/BankAccountTest.java
+++ Banking/src/com/shile/BankAccountTest.java
@@ -1,28 +1,75 @@
 package com.shile;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * @Author: ShiLe
  * @Description:
- * @Date: Created in 11:41 2019/2/14
+ * @Date: Created in 13:42 2019/2/14
  */
-class BankAccountTest {
+public class BankAccountTest {
 
-    @org.junit.jupiter.api.Test
-    void deposit() {
-        fail("This test has yet to be implemented");
+    private BankAccount account;
+    private static int count;
+
+    @org.junit.BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before any test cases. Count = " + count++);
     }
 
-    @org.junit.jupiter.api.Test
-    void withdraw() {
-        fail("This test has yet to be implemented");
-
+    // run before every time run test
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
     }
 
-    @org.junit.jupiter.api.Test
-    void getBalance() {
-        fail("This test has yet to be implemented");
-
+    @org.junit.Test
+    public void deposit() {
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
     }
+
+    @org.junit.Test
+    public void withdraw_branch() {
+        double balance = account.withdraw(600.00, true);
+        assertEquals(400.00, balance, 0);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() {
+        account.withdraw(600.00, false);
+        fail("should have throw a IllegalArgumentException");
+    }
+
+    @org.junit.Test
+    public void getBalance_deposit() {
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void getBalance_withdraw() {
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
+        assertTrue("The account is NOT a checking account", account.isChecking());
+    }
+
+    @org.junit.AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+
+    @org.junit.After
+    public void teardown() {
+        System.out.println("Count = " + count++);
+    }
+
 }
