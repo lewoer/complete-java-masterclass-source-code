417f891fc0c9165b35fa19e7dd5d6eeab86872b5
lewoer <shile4408@126.com>
2019-02-14 11:54:40 +0800

    add Banking for Junit

---
+++ Banking/src/com/shile/BankAccountTest.java
@@ -0,0 +1,28 @@
+package com.shile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author: ShiLe
+ * @Description:
+ * @Date: Created in 11:41 2019/2/14
+ */
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail("This test has yet to be implemented");
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail("This test has yet to be implemented");
+
+    }
+}
---
+++ Banking/src/com/shile/Main.java
@@ -0,0 +1,8 @@
+package com.shile;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("run");
+    }
+}
